2e2f95902fe11b25186f4b7a7d9166c04ed38d40
Osmar Coronel <osmar@Osmars-MacBook-Pro.local>
2019-11-17 16:28:33 -0600

    Added model variables

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import torch.nn as nn
 from utils import Data
 
-from models.main_model import GeneralModel as Model
+from models.resnet import resnet18
 
 
 transform_test = [transforms.ToTensor()]
@@ -24,10 +24,12 @@ batch_size = 100
 no_epoch = 200
 LR = 0.01
 optimizer = optim.SGD(model.parameters(), lr=LR, momentum=0.9)
-criterion = nn.CrossEntropyLoss()
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size=10,gamma=0.5)
+criterion = nn.TripletMarginLoss(
+    margin=1.0
+)  # Only change the params, do not change the criterion.
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
 
-model = Model()
+model = resnet18(pretrained=True)
 
 data = Data(
     batch_size,
@@ -38,11 +40,11 @@ data = Data(
 )
 
 
-start_epoch = 0 # Change me!
+start_epoch = 0  # Change me!
 if os.path("models/trained_models/temp_{}_{}.model".format(model.name, start_epoch)):
-    model = torch.load(
-        "models/trained_models/temp_{}_{}.model".format(model.name, start_epoch)
-    )
+    model.load_state_dict(torch.load(
+        "models/trained_models/temp_{}_{}.pth".format(model.name, start_epoch)
+    ))
     data.train(no_epoch, model, optimizer, start_epoch=start_epoch)
 else:
     data.train(no_epoch, model, optimizer)
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@
 import os
 import torch
 import torch.utils.data
+from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
@@ -38,7 +39,63 @@ def create_val_folder(val_dir):
     return
 
 
-class Data:
+def pil_loader(path):
+    with open(path, "rb") as f:
+        IMG = Image.open(f)
+        return IMG.convert("RGB")
+
+
+class TinyImage(Dataset):
+    def __init__(
+        self,
+        root_dir,
+        *args,
+        train=True,
+        num_classes=200,
+        transform=None,
+        loader=pil_loader,
+        **kwargs
+    ):
+        super(Dataset).__init__(*args, **kwargs)
+        self.train = train
+        self.root_dir = root_dir
+        self.path_to_img = {}
+        self.class_to_path = {}
+        self.all_images = []
+        self.num_classes = num_classes
+        self.loader = loader
+        self.transform = transform
+        for idx, val in enumerate(os.listdir(self.root_dir)):
+            curr_dir = os.path.join(self.root_dir, val, "images")
+            curr_images = list(
+                map(lambda x: os.path.join(curr_dir, x), os.listdir(curr_dir))
+            )
+            self.path_to_img[curr_dir] = curr_images
+            self.class_to_path[idx] = curr_dir
+            self.all_images += list(
+                map(lambda x: (os.path.join(curr_dir, x), idx), curr_images)
+            )
+
+    def __getitem__(self, idx):
+        imp1, im_class = self.all_images[idx]
+        imp2 = np.random.choice(self.path_to_img[self.class_to_path[im_class]])
+        diff_class = np.random.choice(
+            range(0, im_class) + range(im_class + 1, self.num_classes)
+        )
+        imp3 = np.random.choice(self.path_to_img[self.class_to_path[diff_class]])
+
+        images = [imp1, imp2, imp3]
+        for idx, imp in enumerate(images):
+            images[idx] = self.loader(imp)
+            if self.transforms:
+                images[idx] = self.transform(images[idx])
+        return images
+
+    def __len__(self):
+        return len(self.all_images)
+
+
+class Model:
     def __init__(
         self,
         batch_size,
@@ -65,7 +122,8 @@ class Data:
         transform_test = transforms.Compose(transform_test)
 
         train_dir = "./dataset/tiny-imagenet-200/train/"
-        train_dataset = datasets.ImageFolder(train_dir, transform=transform_train)
+        train_dataset = TinyImage(train_dir, transform=transform_train)
+
         self.train_loader = torch.utils.data.DataLoader(
             train_dataset, batch_size=batch_size, shuffle=True, num_workers=8
         )
@@ -76,7 +134,7 @@ class Data:
             val_dir = val_dir + "images/"
         else:
             val_dir = val_dir + "images/"
-        val_dataset = datasets.ImageFolder(val_dir, transform=transform_test)
+        val_dataset = TinyImage(val_dir, transform=transform_test, train=False)
         self.val_loader = torch.utils.data.DataLoader(
             val_dataset, batch_size=batch_size, shuffle=False, num_workers=8
         )
@@ -91,66 +149,69 @@ class Data:
         for epoch in range(start_epoch, self.no_epoch):
             model.train()
             train_accu = []
-            for batch_idx, (X_train_batch, Y_train_batch) in enumerate(
+            for batch_idx, (im1, im2, im3) in enumerate(
                 self.train_loader
             ):
-                X_train_batch, Y_train_batch = (
-                    Variable(X_train_batch).to(device),
-                    Variable(Y_train_batch).to(device),
+                im1, im2, im3 = (
+                    Variable(im1).to(device),
+                    Variable(im2).to(device),
+                    Variable(im3).to(device)
                 )
-                output = model(X_train_batch)
-                loss = self.criterion(output, Y_train_batch)
+                P = model(im1)
+                Q = model(im2)
+                R = model(im3)
+                loss = self.criterion(im1, im2, im3)
                 self.optimizer.zero_grad()
                 loss.backward()
 
                 self.optimizer.step()
-                prediction = output.data.max(1)[1]
-                accuracy = (
-                    float(prediction.eq(Y_train_batch.data).sum())
-                    / float(self.batch_size)
-                ) * 100.0
-                train_accu.append(accuracy)
+                # prediction = output.data.max(1)[1]
+                # accuracy = (
+                #     float(prediction.eq(Y_train_batch.data).sum())
+                #     / float(self.batch_size)
+                # ) * 100.0
+                # train_accu.append(accuracy)
             if self.scheduler is not None:
                 self.scheduler.step()
-            train_acc = np.mean(train_accu)
+            # train_acc = np.mean(train_accu)
 
-            with torch.no_grad():
-                model.eval()
-                test_accu = []
-                for batch_idx, (X_test_batch, Y_test_batch) in enumerate(
-                    self.val_loader
-                ):
-                    X_test_batch, Y_test_batch = (
-                        Variable(X_test_batch).to(device),
-                        Variable(Y_test_batch).to(device),
-                    )
-                    output = model(X_test_batch)
-                    prediction = output.data.max(1)[1]
-                    accuracy = (
-                        float(prediction.eq(Y_test_batch.data).sum())
-                        / float(self.batch_size)
-                    ) * 100.0
-                    test_accu = []
-                test_acc = np.mean(test_accu)
-            train_acc_list.append(train_acc)
-            test_acc_list.append(test_acc)
-            time_list.append(time.time() - start_time)
+            # with torch.no_grad():
+            #     model.eval()
+            #     test_accu = []
+            #     for batch_idx, (X_test_batch, Y_test_batch) in enumerate(
+            #         self.val_loader
+            #     ):
+            #         X_test_batch, Y_test_batch = (
+            #             Variable(X_test_batch).to(device),
+            #             Variable(Y_test_batch).to(device),
+            #         )
+            #         output = model(X_test_batch)
+            #         prediction = output.data.max(1)[1]
+            #         accuracy = (
+            #             float(prediction.eq(Y_test_batch.data).sum())
+            #             / float(self.batch_size)
+            #         ) * 100.0
+            #         test_accu = []
+            #     test_acc = np.mean(test_accu)
+            # train_acc_list.append(train_acc)
+            # test_acc_list.append(test_acc)
+            # time_list.append(time.time() - start_time)
 
-            if (epoch + 1) % 15 == 0 and should_save:
+            if (epoch + 1) % 1 == 0 and should_save:
                 torch.save(
-                    model,
-                    "models/trained_models/temp_{}_{}.model".format(model.name, epoch),
+                    model.state_dict(),
+                    "models/trained_models/temp_{}_{}.pth".format(model.name, epoch),
                 )
                 torch.save(
                     optimizer,
                     "models/trained_models/temp_{}_{}.state".format(model.name, epoch),
                 )
-                data = [train_acc_list, test_acc_list]
-                data = np.asarray(data)
-                np.save(
-                    "models/trained_models/temp_{}_{}.npy".format(model.name, epoch),
-                    data,
-                )
+                # data = [train_acc_list, test_acc_list]
+                # data = np.asarray(data)
+                # np.save(
+                #     "models/trained_models/temp_{}_{}.npy".format(model.name, epoch),
+                #     data,
+                # )
         if should_save:
-            torch.save(model, "models/trained_models/{}.model".format(model.name))
-            np.save("models/trained_models/{}_{}.npy".format(model.name, epoch), data)
+            torch.save(model.state_dict(), "models/trained_models/{}.pth".format(model.name))
+            # np.save("models/trained_models/{}_{}.npy".format(model.name, epoch), data)
