fd1f1ebb404ec1cdca4e8a547cef6e5b34a37dfd
Blue Waters Trainee 353 <tra353@h2ologin2.ncsa.illinois.edu>
2019-11-26 22:40:13 -0600

    Removed embeddings

--- models/resnet.py
+++ models/resnet.py
@@ -108,10 +108,11 @@ model_urls = {
 
 
 def resnet18(pretrained=True):
+    #model = torchvision.models.resnet.ResNet(torchvision.models.resnet.BasicBlock, [2, 2, 2, 2])
     model = torchvision.models.resnet.ResNet(torchvision.models.resnet.BasicBlock, [2, 2, 2, 2])
     if pretrained:
         model.load_state_dict(
             model_zoo.load_url(model_urls["resnet18"], model_dir="~/scratch/HW4_Model")
         )
-    model.name = "ResNet18"
+    model.name = "ResNet18-Torch"
     return model
